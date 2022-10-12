cff68907678d97114e24a4ab4f5edffe9b73723d
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-12 01:24:57 -0300

    Update troco.py

--- troco.py
+++ troco.py
@@ -1,11 +1,16 @@
 valores=[1,2,3,4,5,6,7,10,15,20,25,30,49,50,74,99]
 memo=dict()
-def troco(valor):
+def troco(valor=0):
     if valor < 0:
-        return float('infinity')
+        return None
     if valor == 0:
         return 0
     if memo.get(valor) != None:
         return memo[valor]
-    memo[valor]=1+min([troco(valor-i) for i in valores])
+    quantidade=list()
+    for i in valores:
+        if valor < i:
+            break
+        quantidade.append(troco(valor-i))
+    memo[valor]=1+min(quantidade)
     return memo[valor]
